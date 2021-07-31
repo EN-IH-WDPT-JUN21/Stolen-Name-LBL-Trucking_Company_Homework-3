366405035ec9e641ac7e2be73e13847ec266edf0
Lee Dawson <lee.dawson1982@hotmail.co.uk>
2021-07-31 10:00:10 +0100

    add sales rep

---
+++ src/main/java/com/ironhack/stolen_name_trucking_company_homework_3/dao/SalesRep.java
@@ -0,0 +1,25 @@
+package com.ironhack.stolen_name_trucking_company_homework_3.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalesRep {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="sales_rep_id")
+    private Long salesRepId;
+
+    @Column(name="sales_rep_name")
+    private String name;
+
+}
